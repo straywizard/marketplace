86ed97c969b01a311b39a717f103d9eccc9aee92
yvva <olimpac2005@gmail.com>
2025-04-14 00:02:33 +0300

    ай ай ай красота какая

--- MarketplaceUI/Products.swift
+++ MarketplaceUI/Products.swift
@@ -11,11 +11,12 @@ import Observation
 
 
 struct Product {
+    //let id: UUID = UUID()
     var name: String = "-"
     var price: String = "-"
     var image: String = "No image"
     var description: String = "No description"
-    let id: Int
+    let productID: Int
     
     var isInCart: Bool = false
     var isFavourite: Bool = false
@@ -26,11 +27,11 @@ struct Product {
 @Observable
 class Products {
         var ProductList: [Product] = [
-            Product(name: "IPhone SE 2020", price: "250", image: "iphonese", id: 111111),
-            Product(name: "MacBook M2", price: "1000", image: "macbookm2", description: "soyboy attribute", id: 222222),
-            Product(name: "Apple Watch Series 10", price: "400", image: "applewatchseries10", description: "100% useless", id: 333333),
-            Product(name: "Soy Milk", price: "20", image: "soymilk", description: "my wife's boyfriend LOVES this liquid", id: 444444),
-            Product(name: "One Robux", price: "799", image: "robux", description: "one robux", id: 555555)
+            Product(name: "IPhone SE 2020", price: "250", image: "iphonese", productID: 111111),
+            Product(name: "MacBook M2", price: "1000", image: "macbookm2", description: "soyboy attribute", productID: 222222),
+            Product(name: "Apple Watch Series 10", price: "400", image: "applewatchseries10", description: "100% useless", productID: 333333),
+            Product(name: "Soy Milk", price: "20", image: "soymilk", description: "my wife's boyfriend LOVES this liquid", productID: 444444),
+            Product(name: "One Robux", price: "799", image: "robux", description: "one robux", productID: 555555)
     ]
 }
 
--- MarketplaceUI/Views/ProductPageView.swift
+++ MarketplaceUI/Views/ProductPageView.swift
@@ -5,3 +5,27 @@
 //  Created by Ivan Loktionov on 08.04.2025.
 //
 
+import SwiftUI
+
+struct ProductPageView: View {
+    var ProductData: Product
+    var body: some View {
+        VStack {
+            Image("\(ProductData.image)")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                
+            
+            HStack {
+                Text(ProductData.name)
+                Text("\(ProductData.price)$")
+                    
+            }
+            Text(ProductData.description)
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    ProductPageView(ProductData: Product(name: "IPhone SE 2020", price: "250", image: "iphonese", productID: 111111))
+}
